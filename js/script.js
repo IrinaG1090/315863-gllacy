d894ff4b862cb171f6876297e1ca6974af6b9824
Irina <k-10@inbox.ru>
2018-04-01 14:08:25 +0300

    последние исправления

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
+var link = document.querySelector(".map_form_btn");
 var popup = document.querySelector(".feedback-show");
 var close = document.querySelector(".feedback-close");
 
-popup = classList.add("feedback-active");
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+popup.classList.add("feedback-active");
+});
+
+
 
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
 
-close = classList.remove("feedback-active");
+popup.classList.remove("feedback-active");
 
 });
-
