330743182c9fd41f292172cc0d538aae51008a01
Irina <k-10@inbox.ru>
2018-04-01 08:46:58 +0300

    доделаны 2 страницы

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var popup = document.querySelector(".feedback-show");
+var close = document.querySelector(".feedback-close");
+
+popup = classList.add("feedback-active");
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+close = classList.remove("feedback-active");
+
+});
+
